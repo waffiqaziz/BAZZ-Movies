a2a1b53a2528d8bbeb7ee2db5a249732b9756438
WaffiqA <waffiq.a2002@gmail.com>
2025-01-06 09:59:39 +0700

    docs: add description to helper functions

--- core/favoritewatchlist/src/main/kotlin/com/waffiq/bazz_movies/core/favoritewatchlist/utils/helpers/SnackBarUserLoginData.kt
+++ core/favoritewatchlist/src/main/kotlin/com/waffiq/bazz_movies/core/favoritewatchlist/utils/helpers/SnackBarUserLoginData.kt
@@ -3,9 +3,27 @@ package com.waffiq.bazz_movies.core.favoritewatchlist.utils.helpers
 import com.waffiq.bazz_movies.core.domain.FavoriteModel
 import com.waffiq.bazz_movies.core.domain.WatchlistModel
 
+/**
+ * Data class representing the information required to display a Snackbar
+ * related to the user's login state, particularly focusing on their favorites
+ * and watchlist.
+ *
+ * This class is used to encapsulate the outcome of a user login action,
+ * including success status, title also as error message, and additional data related
+ * to the user's favorite and watchlist models.
+ *
+ * @param isSuccess Boolean indicating whether the login operation was successful.
+ * @param title A message to be displayed in the Snackbar. This can either be an
+ *              error message or a success message based on the value of `isSuccess`.
+ * @param favoriteModel An optional [FavoriteModel] object containing the user's
+ *                      favorite items. This can be null if no favorites are involved.
+ * @param watchlistModel An optional [WatchlistModel] object containing the
+ *                       user's watchlist items. This can be null if no watchlist
+ *                       is involved.
+ */
 data class SnackBarUserLoginData(
-  val isSuccess: Boolean,
-  val title: String, // also as message error
-  val favoriteModel: FavoriteModel?,
-  val watchlistModel: WatchlistModel?
+  val isSuccess: Boolean, // indicates whether the login was successful or not
+  val title: String, // the title/message to be shown in the Snackbar, could be an error or success message
+  val favoriteModel: FavoriteModel?, // optional data related to the user's favorites, may be null if not relevant
+  val watchlistModel: WatchlistModel? // optional data related to the user's watchlist, may be null if not relevant
 )
