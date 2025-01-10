e59e7ce337e7779f6ede78588591199f2b2206c8
WaffiqA <waffiq.a2002@gmail.com>
2025-01-10 11:54:56 +0700

    chore: use version catalog as source of truth for plugin IDs

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
   alias(libs.plugins.android.application) apply false
   alias(libs.plugins.android.library) apply false
+  alias(libs.plugins.android.test) apply false
   alias(libs.plugins.kotlin.android) apply false
   alias(libs.plugins.kotlin.jvm) apply false
   alias(libs.plugins.gms.googleServices) apply false
@@ -10,5 +11,6 @@ plugins {
   alias(libs.plugins.dependencyGuard) apply false
   alias(libs.plugins.hilt) apply false
   alias(libs.plugins.detekt) apply false
+  alias(libs.plugins.room) apply false
   alias(libs.plugins.ksp) apply false
 }
