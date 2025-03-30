2f8f403c0fa9adea6fece98daa9446bf9929c17b
WaffiqA <waffiq.a2002@gmail.com>
2025-03-30 22:53:06 +0700

    refactor: reduce duplicate on LocalDataSourceTest

--- core/database/src/test/kotlin/com/waffiq/bazz_movies/core/database/data/repository/DatabaseRepositoryImplTest.kt
+++ core/database/src/test/kotlin/com/waffiq/bazz_movies/core/database/data/repository/DatabaseRepositoryImplTest.kt
@@ -2,7 +2,9 @@ package com.waffiq.bazz_movies.core.database.data.repository
 
 import app.cash.turbine.test
 import com.waffiq.bazz_movies.core.database.data.datasource.LocalDataSource
-import com.waffiq.bazz_movies.core.database.data.model.FavoriteEntity
+import com.waffiq.bazz_movies.core.database.testdummy.DummyData.favoriteMovie
+import com.waffiq.bazz_movies.core.database.testdummy.DummyData.favoriteMovieEntity
+import com.waffiq.bazz_movies.core.database.testdummy.DummyData.favoriteTvEntity
 import com.waffiq.bazz_movies.core.database.utils.DatabaseMapper.toFavoriteEntity
 import com.waffiq.bazz_movies.core.database.utils.DbResult
 import com.waffiq.bazz_movies.core.domain.Favorite
@@ -20,53 +22,6 @@ import org.junit.Rule
 import org.junit.Test
 
 class DatabaseRepositoryImplTest {
-
-  private val favoriteTvEntity = FavoriteEntity(
-    id = 1,
-    mediaId = 103,
-    mediaType = "tv",
-    genre = "Drama",
-    backDrop = "backdrop3",
-    poster = "poster3",
-    overview = "overview3",
-    title = "Show1",
-    releaseDate = "2023-01-03",
-    popularity = 9.0,
-    rating = 4.8f,
-    isFavorite = false,
-    isWatchlist = true
-  )
-  private val favoriteMovieEntity = FavoriteEntity(
-    id = 1,
-    mediaId = 101,
-    mediaType = "movie",
-    genre = "Action",
-    backDrop = "backdrop1",
-    poster = "poster1",
-    overview = "overview1",
-    title = "Movie1",
-    releaseDate = "2023-01-01",
-    popularity = 8.5,
-    rating = 4.5f,
-    isFavorite = false,
-    isWatchlist = true
-  )
-
-  private val favoriteMovie = Favorite(
-    id = 0,
-    mediaId = 101,
-    mediaType = "movie",
-    genre = "Action",
-    backDrop = "",
-    poster = "",
-    overview = "",
-    title = "Movie1",
-    releaseDate = "",
-    popularity = 0.0,
-    rating = 0f,
-    isFavorite = true,
-    isWatchlist = false
-  )
 
   private lateinit var repository: DatabaseRepositoryImpl
   private val localDataSource: LocalDataSource = mockk()
