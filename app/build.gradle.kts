0bc8f481974c3281c32ac62c0c041f9c5296d84c
WaffiqA <waffiq.a2002@gmail.com>
2025-11-30 07:59:49 +0700

    chore: separate build use flavor
    - Build flavor withFirebase
    - Build flavor withoutFirebase
    - Use shared flavor
    - Remove implicit PendingIntents
    - Only show notification requirement for android 33 and up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
   alias(libs.plugins.bazzmovies.glide)
   alias(libs.plugins.bazzmovies.hilt)
   alias(libs.plugins.bazzmovies.hilt.test)
-  alias(libs.plugins.gms.googleServices)
   id("kotlin-parcelize")
 }
 
@@ -49,7 +48,6 @@ dependencies {
 
   // third-party library
   implementation(libs.expandable.textview)
-  implementation(libs.firebase.messaging)
 
   // testing
   testImplementation(libs.mockk)
@@ -71,8 +69,11 @@ configurations.androidTestImplementation.configure {
 }
 
 dependencyGuard {
-  configuration("releaseCompileClasspath")
-  configuration("releaseRuntimeClasspath")
+  configuration("withFirebaseReleaseCompileClasspath")
+  configuration("withFirebaseReleaseRuntimeClasspath")
+
+  configuration("withoutFirebaseReleaseCompileClasspath")
+  configuration("withoutFirebaseReleaseRuntimeClasspath")
 }
 
 // Disabling detekt from the check task
--- app/src/withFirebase/kotlin/com/waffiq/bazz_movies/cloudmessage/FirebaseCloudMessagingService.kt
+++ app/src/withFirebase/kotlin/com/waffiq/bazz_movies/cloudmessage/FirebaseCloudMessagingService.kt
@@ -2,16 +2,21 @@ package com.waffiq.bazz_movies.cloudmessage
 
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.ComponentName
 import android.content.Intent
+import android.util.Log
 import androidx.core.app.NotificationCompat
-import androidx.core.content.ContextCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.waffiq.bazz_movies.RoutingActivity
-import com.waffiq.bazz_movies.core.designsystem.R.drawable.ic_bazz_logo
+import com.waffiq.bazz_movies.core.designsystem.R.drawable.ic_bazz_monochrome
 import com.waffiq.bazz_movies.core.designsystem.R.string.default_notification_channel_id
 
 class FirebaseCloudMessagingService : FirebaseMessagingService() {
+
+  override fun onNewToken(token: String) {
+    Log.d("FirebaseCloudMessagingService", "Refreshed token: $token")
+  }
 
   override fun onMessageReceived(remoteMessage: RemoteMessage) {
     val title = remoteMessage.notification?.title ?: "New Message"
@@ -22,21 +27,22 @@ class FirebaseCloudMessagingService : FirebaseMessagingService() {
 
   private fun showNotification(title: String, message: String) {
     val intent = Intent(this, RoutingActivity::class.java).apply {
-      flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+      flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+      component = ComponentName(packageName, RoutingActivity::class.java.name)
     }
 
     val pendingIntent = PendingIntent.getActivity(
       this,
       NOTIFICATION_ID,
       intent,
-      PendingIntent.FLAG_IMMUTABLE
+      PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_ONE_SHOT
     )
 
     val builder = NotificationCompat.Builder(
       this,
-      ContextCompat.getString(this, default_notification_channel_id)
+      getString(default_notification_channel_id)
     )
-      .setSmallIcon(ic_bazz_logo)
+      .setSmallIcon(ic_bazz_monochrome)
       .setContentTitle(title)
       .setContentText(message)
       .setAutoCancel(true)
