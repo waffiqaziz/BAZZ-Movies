d1dee675f23a152e06ee08e5fc124eb31202d3a7
WaffiqA <waffiq.a2002@gmail.com>
2025-12-05 06:47:57 +0700

    test: update helper test to handle GradientDrawable on app bar scroll

--- core/uihelper/src/test/kotlin/com/waffiq/bazz_movies/core/uihelper/testutils/Helper.kt
+++ core/uihelper/src/test/kotlin/com/waffiq/bazz_movies/core/uihelper/testutils/Helper.kt
@@ -1,6 +1,8 @@
 package com.waffiq.bazz_movies.core.uihelper.testutils
 
+import android.graphics.Paint
 import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.GradientDrawable
 import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.shape.MaterialShapeDrawable
 
@@ -10,6 +12,17 @@ object Helper {
     return when (val background = appBarLayout.background) {
       is ColorDrawable -> background.color
       is MaterialShapeDrawable -> background.fillColor?.defaultColor
+      is GradientDrawable -> {
+        try {
+          //  uses reflection because GradientDrawable hides color field
+          val field = GradientDrawable::class.java.getDeclaredField("mFillPaint")
+          field.isAccessible = true
+          val paint = field.get(background) as? Paint
+          paint?.color
+        } catch (_: Exception) {
+          null
+        }
+      }
       else -> null
     }
   }
