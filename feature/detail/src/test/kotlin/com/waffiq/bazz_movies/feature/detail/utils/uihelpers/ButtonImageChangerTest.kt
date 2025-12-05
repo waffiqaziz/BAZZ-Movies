45b4975be27bf254c56f45ee0517d68936cc1d64
WaffiqA <waffiq.a2002@gmail.com>
2025-12-06 06:27:44 +0700

    refactor: use shared button background changes

--- feature/detail/src/test/kotlin/com/waffiq/bazz_movies/feature/detail/utils/uihelpers/ButtonImageChangerTest.kt
+++ feature/detail/src/test/kotlin/com/waffiq/bazz_movies/feature/detail/utils/uihelpers/ButtonImageChangerTest.kt
@@ -3,12 +3,9 @@ package com.waffiq.bazz_movies.feature.detail.utils.uihelpers
 import android.content.Context
 import android.widget.ImageButton
 import androidx.test.core.app.ApplicationProvider
-import com.waffiq.bazz_movies.core.designsystem.R.drawable.ic_bookmark
-import com.waffiq.bazz_movies.core.designsystem.R.drawable.ic_bookmark_selected
 import com.waffiq.bazz_movies.core.designsystem.R.drawable.ic_hearth
 import com.waffiq.bazz_movies.core.designsystem.R.drawable.ic_hearth_selected
-import com.waffiq.bazz_movies.feature.detail.utils.uihelpers.ButtonImageChanger.changeBtnFavoriteBG
-import com.waffiq.bazz_movies.feature.detail.utils.uihelpers.ButtonImageChanger.changeBtnWatchlistBG
+import com.waffiq.bazz_movies.feature.detail.utils.uihelpers.ButtonImageChanger.changeBtnAction
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
@@ -32,50 +29,41 @@ class ButtonImageChangerTest {
   }
 
   @Test
-  fun changeBtnWatchlistBG_withDifferentTag_setsNewTagAndDrawable() {
+  fun changeBtnAction_withDifferentTag_setsNewTagAndDrawable() {
     imageButton.tag = null
 
-    changeBtnWatchlistBG(imageButton, isActivated = true)
-    assertEquals(ic_bookmark_selected, imageButton.tag)
-  }
-
-  @Test
-  fun changeBtnWatchlistBG_isNotActive_setsNewDrawable() {
-    imageButton.tag = null
-
-    changeBtnWatchlistBG(imageButton, isActivated = false)
-    assertEquals(ic_bookmark, imageButton.tag)
-  }
-
-  @Test
-  fun changeBtnWatchlistBG_withSameTag_doesNothing() {
-    imageButton.tag = ic_bookmark_selected
-
-    changeBtnWatchlistBG(imageButton, isActivated = true)
-    assertEquals(ic_bookmark_selected, imageButton.tag)
-  }
-
-  @Test
-  fun changeBtnFavoriteBG_withDifferentTag_setsNewTagAndDrawable() {
-    imageButton.tag = null
-
-    changeBtnFavoriteBG(imageButton, isActivated = false)
+    changeBtnAction(
+      button = imageButton,
+      isActivated = false,
+      iconActive = ic_hearth_selected,
+      iconInactive = ic_hearth
+    )
     assertEquals(ic_hearth, imageButton.tag)
   }
 
   @Test
-  fun changeBtnFavoriteBG_isActive_setsNewDrawable() {
-    imageButton.tag = ic_hearth_selected
+  fun changeBtnAction_isActive_setsNewDrawable() {
+    imageButton.tag = null
 
-    changeBtnFavoriteBG(imageButton, isActivated = true)
+    changeBtnAction(
+      button = imageButton,
+      isActivated = true,
+      iconActive = ic_hearth_selected,
+      iconInactive = ic_hearth
+    )
     assertEquals(ic_hearth_selected, imageButton.tag)
   }
 
   @Test
-  fun changeBtnFavoriteBG_withSameTag_doesNothing() {
-    imageButton.tag = ic_hearth
+  fun changeBtnAction_withSameTag_doesNothing() {
+    imageButton.tag = ic_hearth_selected
 
-    changeBtnFavoriteBG(imageButton, isActivated = false)
-    assertEquals(ic_hearth, imageButton.tag)
+    changeBtnAction(
+      button = imageButton,
+      isActivated = true,
+      iconActive = ic_hearth_selected,
+      iconInactive = ic_hearth
+    )
+    assertEquals(ic_hearth_selected, imageButton.tag)
   }
 }
