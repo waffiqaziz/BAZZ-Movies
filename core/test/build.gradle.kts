8168cd0b2e697c545e4aae58d702049f3ab2ef0b
WaffiqA <waffiq.a2002@gmail.com>
2025-12-06 11:38:53 +0700

    chore: create shared test convention plugin

--- core/test/build.gradle.kts
+++ core/test/build.gradle.kts
@@ -1,18 +1,14 @@
 plugins {
   alias(libs.plugins.bazzmovies.android.library)
+  alias(libs.plugins.bazzmovies.shared.test)
 }
 
-android{
-  namespace = "com.waffiq.bazz_movies.core.test"
-
-  testOptions {
-    unitTests.apply {
-      isIncludeAndroidResources = false
-    }
-  }
-}
+android.namespace = "com.waffiq.bazz_movies.core.test"
 
 dependencies {
+  implementation(project(":core:domain"))
+  implementation(project(":core:network"))
+
   implementation(libs.androidx.appcompat.resources)
   implementation(libs.androidx.core.ktx)
   implementation(libs.androidx.recyclerview)
