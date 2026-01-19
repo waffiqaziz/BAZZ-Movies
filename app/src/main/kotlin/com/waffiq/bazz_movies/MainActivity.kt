c15b167d4db96e8fbdc74708d0b0971e94a8a80a
WaffiqA <waffiq.a2002@gmail.com>
2026-01-19 08:36:05 +0700

    chore: improvement on bottom navigation icon
    - Use filled icon for selected page except search page
    - Animated icon for search page
    - Reduce space between two button action on detail page

--- app/src/main/kotlin/com/waffiq/bazz_movies/MainActivity.kt
+++ app/src/main/kotlin/com/waffiq/bazz_movies/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.waffiq.bazz_movies
 
+import android.animation.AnimatorInflater
 import android.graphics.Color
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +14,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.waffiq.bazz_movies.R.id.nav_host_fragment_activity_home
 import com.waffiq.bazz_movies.R.id.navigation_search
@@ -44,7 +49,7 @@ class MainActivity : AppCompatActivity() {
       v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
         topMargin = insets.top
         leftMargin = insets.left
-        bottomMargin = insets.bottom
+        bottomMargin = insets.bottom - 64
         rightMargin = insets.right
       }
       WindowInsetsCompat.CONSUMED
@@ -59,12 +64,48 @@ class MainActivity : AppCompatActivity() {
     // setup the BottomNavigationView with NavController
     binding.bottomNavigation.setupWithNavController(navHostFragment.navController)
 
+    binding.bottomNavigation.setOnItemSelectedListener { menuItem ->
+      if (menuItem.itemId == navigation_search) {
+        animateSearchIcon(menuItem)
+      }
+      NavigationUI.onNavDestinationSelected(menuItem, navHostFragment.navController)
+    }
+
     // handle search button
     binding.bottomNavigation.setOnItemReselectedListener { menuItem ->
       if (menuItem.itemId == navigation_search) {
+        animateSearchIcon(menuItem)
         supportFragmentManager.setFragmentResult("open_search_view", Bundle.EMPTY)
       }
     }
+  }
+
+  private fun animateSearchIcon(item: MenuItem) {
+    val menuView = binding.bottomNavigation.getChildAt(0) as ViewGroup
+    val itemView = menuView.findViewById<View>(item.itemId)
+
+    val iconView = findImageView(itemView) ?: return
+    iconView.cameraDistance = iconView.resources.displayMetrics.density * 8000f
+    iconView.pivotX = iconView.width / 2f
+    iconView.pivotY = iconView.height / 2f
+
+    AnimatorInflater.loadAnimator(
+      iconView.context,
+      R.animator.search_rotate_pop
+    ).apply {
+      setTarget(iconView)
+      start()
+    }
+  }
+
+  private fun findImageView(view: View): ImageView? {
+    if (view is ImageView) return view
+    if (view is ViewGroup) {
+      for (i in 0 until view.childCount) {
+        findImageView(view.getChildAt(i))?.let { return it }
+      }
+    }
+    return null
   }
 
   override fun onDestroy() {
