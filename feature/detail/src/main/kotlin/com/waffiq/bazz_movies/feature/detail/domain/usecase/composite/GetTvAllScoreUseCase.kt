a1348b7246e6d545d010a61ba2d1938878d0c103
WaffiqA <waffiq.a2002@gmail.com>
2026-01-25 00:08:51 +0700

    chore: use interface function for single function

--- feature/detail/src/main/kotlin/com/waffiq/bazz_movies/feature/detail/domain/usecase/composite/GetTvAllScoreUseCase.kt
+++ feature/detail/src/main/kotlin/com/waffiq/bazz_movies/feature/detail/domain/usecase/composite/GetTvAllScoreUseCase.kt
@@ -4,6 +4,6 @@ import com.waffiq.bazz_movies.core.domain.Outcome
 import com.waffiq.bazz_movies.feature.detail.domain.model.omdb.OMDbDetails
 import kotlinx.coroutines.flow.Flow
 
-interface GetTvAllScoreUseCase {
+fun interface GetTvAllScoreUseCase {
   fun getTvAllScore(tvId: Int): Flow<Outcome<OMDbDetails>>
 }
