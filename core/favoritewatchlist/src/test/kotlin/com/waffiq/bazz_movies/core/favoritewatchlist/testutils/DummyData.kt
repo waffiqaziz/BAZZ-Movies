174ebc586f13d329373cdfbee457d53a9777d1e1
WaffiqA <waffiq.a2002@gmail.com>
2026-02-13 06:54:42 +0700

    test: use const for data test data

--- core/favoritewatchlist/src/test/kotlin/com/waffiq/bazz_movies/core/favoritewatchlist/testutils/DummyData.kt
+++ core/favoritewatchlist/src/test/kotlin/com/waffiq/bazz_movies/core/favoritewatchlist/testutils/DummyData.kt
@@ -4,8 +4,8 @@ import com.waffiq.bazz_movies.core.domain.Favorite
 
 object DummyData {
 
-  val indonesianMovie = "Indonesian Movie"
-  val indonesianMovie2 = "Indonesian Movie 2"
+  const val INA_MOVIE_TITLE = "Indonesian Movie"
+  const val INA_MOVIE_TITLE2 = "Indonesian Movie 2"
   val favorite = Favorite(
     id = 1,
     mediaId = 1,
@@ -14,7 +14,7 @@ object DummyData {
     backDrop = "backdrop",
     poster = "poster",
     overview = "overview",
-    title = indonesianMovie,
+    title = INA_MOVIE_TITLE,
     releaseDate = "1979-04-04",
     popularity = 214.0,
     rating = 9.0f,
