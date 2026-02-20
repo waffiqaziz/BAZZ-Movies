6d4c92f6e7d84d10149cd17e938a94de079e7bbd
WaffiqA <waffiq.a2002@gmail.com>
2026-02-20 07:56:40 +0700

    test: keep unused function

--- core/uihelper/src/test/kotlin/com/waffiq/bazz_movies/core/uihelper/testutils/ShadowSettingsSecure.kt
+++ core/uihelper/src/test/kotlin/com/waffiq/bazz_movies/core/uihelper/testutils/ShadowSettingsSecure.kt
@@ -1,11 +1,17 @@
 package com.waffiq.bazz_movies.core.uihelper.testutils
 
 import android.provider.Settings
+import org.robolectric.annotation.Implementation
 import org.robolectric.annotation.Implements
 
 @Implements(Settings.Secure::class)
 class ShadowSettingsSecure {
   private val secureSettings = mutableMapOf<String, Int>()
+
+  @Suppress("unused")
+  @Implementation
+  fun getInt(name: String): Int =
+    secureSettings[name] ?: throw Settings.SettingNotFoundException(name)
 
   fun setSecureSetting(name: String, value: Int) {
     secureSettings[name] = value
