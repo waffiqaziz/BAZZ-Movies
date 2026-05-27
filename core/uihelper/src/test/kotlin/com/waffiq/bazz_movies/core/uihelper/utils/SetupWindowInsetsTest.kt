a9d3b942bed1c2a40e48d87c740a32f0a3c0be26
WaffiqA <waffiq.a2002@gmail.com>
2026-05-27 15:57:01 +0700

    test: remove duplicate test

--- core/uihelper/src/test/kotlin/com/waffiq/bazz_movies/core/uihelper/utils/SetupWindowInsetsTest.kt
+++ core/uihelper/src/test/kotlin/com/waffiq/bazz_movies/core/uihelper/utils/SetupWindowInsetsTest.kt
@@ -47,68 +47,9 @@ class SetupWindowInsetsTest {
     val params = view.layoutParams as ViewGroup.MarginLayoutParams
     assertEquals(16, params.leftMargin)
     assertEquals(24, params.rightMargin)
-  }
 
-  @Test
-  fun setupWindowInsets_whenInsetsApplied_doesNotSetTopOrBottomMargins() {
-    view.setupWindowInsets()
-
-    val insetsType = WindowInsetsCompat.Type.systemBars() or
-      WindowInsetsCompat.Type.displayCutout()
-
-    val fakeWindowInsets = WindowInsetsCompat.Builder()
-      .setInsets(insetsType, androidx.core.graphics.Insets.of(8, 100, 12, 200))
-      .build()
-
-    ViewCompat.dispatchApplyWindowInsets(view, fakeWindowInsets)
-
-    val params = view.layoutParams as ViewGroup.MarginLayoutParams
+    // does not set top or bottom margins
     assertEquals(0, params.topMargin)
     assertEquals(0, params.bottomMargin)
-  }
-
-  @Test
-  fun setupWindowInsets_whenInsetsApplied_returnsInsetsUnconsumed() {
-    var capturedInsets: WindowInsetsCompat? = null
-
-    // Wrap BEFORE calling setupWindowInsets so we can observe the return value
-    ViewCompat.setOnApplyWindowInsetsListener(view) { v, insets ->
-      // Temporarily clear the listener to avoid infinite recursion,
-      // then call setupWindowInsets's listener manually
-      ViewCompat.setOnApplyWindowInsetsListener(v, null)
-      v.setupWindowInsets()
-      capturedInsets = ViewCompat.dispatchApplyWindowInsets(v, insets)
-      capturedInsets
-    }
-
-    val insetsType = WindowInsetsCompat.Type.systemBars() or
-      WindowInsetsCompat.Type.displayCutout()
-
-    val fakeWindowInsets = WindowInsetsCompat.Builder()
-      .setInsets(insetsType, androidx.core.graphics.Insets.of(16, 24, 16, 24))
-      .build()
-
-    ViewCompat.dispatchApplyWindowInsets(view, fakeWindowInsets)
-
-    assertEquals(16, capturedInsets?.getInsets(insetsType)?.left)
-    assertEquals(16, capturedInsets?.getInsets(insetsType)?.right)
-  }
-
-  @Test
-  fun setupWindowInsets_whenInsetsAreZero_setsZeroMargins() {
-    view.setupWindowInsets()
-
-    val insetsType = WindowInsetsCompat.Type.systemBars() or
-      WindowInsetsCompat.Type.displayCutout()
-
-    val fakeWindowInsets = WindowInsetsCompat.Builder()
-      .setInsets(insetsType, androidx.core.graphics.Insets.of(0, 0, 0, 0))
-      .build()
-
-    ViewCompat.dispatchApplyWindowInsets(view, fakeWindowInsets)
-
-    val params = view.layoutParams as ViewGroup.MarginLayoutParams
-    assertEquals(0, params.leftMargin)
-    assertEquals(0, params.rightMargin)
   }
 }
