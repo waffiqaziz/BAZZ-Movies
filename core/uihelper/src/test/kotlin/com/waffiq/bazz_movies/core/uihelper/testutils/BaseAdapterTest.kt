f8329af95931b505d7b52696196be1091cfbc9b9
WaffiqA <waffiq.a2002@gmail.com>
2026-07-04 05:43:52 +0700

    test: remove unused mock object

--- core/uihelper/src/test/kotlin/com/waffiq/bazz_movies/core/uihelper/testutils/BaseAdapterTest.kt
+++ core/uihelper/src/test/kotlin/com/waffiq/bazz_movies/core/uihelper/testutils/BaseAdapterTest.kt
@@ -3,7 +3,6 @@ package com.waffiq.bazz_movies.core.uihelper.testutils
 import android.content.Context
 import android.view.LayoutInflater
 import android.widget.FrameLayout
-import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ApplicationProvider
 import com.waffiq.bazz_movies.core.designsystem.R.style.Base_Theme_BAZZ_movies
 import com.waffiq.bazz_movies.navigation.INavigator
@@ -13,9 +12,6 @@ import io.mockk.just
 import io.mockk.mockk
 import org.junit.Before
 import org.junit.runner.RunWith
-import org.mockito.Mock
-import org.mockito.Mockito.mock
-import org.mockito.MockitoAnnotations
 import org.robolectric.RobolectricTestRunner
 
 @RunWith(RobolectricTestRunner::class)
@@ -25,18 +21,13 @@ abstract class BaseAdapterTest {
   protected lateinit var parent: FrameLayout
   protected lateinit var inflater: LayoutInflater
 
-  @Mock
-  protected lateinit var recyclerView: RecyclerView
-
   protected val navigator: INavigator = mockk()
 
   @Before
   open fun setup() {
-    MockitoAnnotations.openMocks(this)
     context = ApplicationProvider.getApplicationContext<Context>().apply {
       setTheme(Base_Theme_BAZZ_movies) // set the theme
     }
-    recyclerView = mock(RecyclerView::class.java)
     parent = FrameLayout(context)
     inflater = LayoutInflater.from(context)
 
