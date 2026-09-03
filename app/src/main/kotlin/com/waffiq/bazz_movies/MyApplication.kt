96d1f4e0a4da693b5ae60c1bf89c01142859757c
WaffiqA <waffiq.a2002@gmail.com>
2026-09-03 22:23:29 +0700

    chore: set English as the default locale
    - Set English as the default locale since the app currently only supports English
    - Use the default locale for proper plural string handling

--- app/src/main/kotlin/com/waffiq/bazz_movies/MyApplication.kt
+++ app/src/main/kotlin/com/waffiq/bazz_movies/MyApplication.kt
@@ -1,7 +1,18 @@
 package com.waffiq.bazz_movies
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-open class MyApplication : Application()
+open class MyApplication : Application() {
+
+  override fun onCreate() {
+    super.onCreate()
+
+    AppCompatDelegate.setApplicationLocales(
+      LocaleListCompat.forLanguageTags("en"),
+    )
+  }
+}
